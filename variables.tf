89f0c3a68d1f958aadcd315a6c3501017a985385
dinomite505 <dino.milje@gmail.com>
2024-02-14 12:54:54 +0100

    modified .tf files

---
+++ variables.tf
@@ -0,0 +1,23 @@
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+}
+variable "github_owner" {
+  description = "GitHub repository owner"
+  type        = string
+}
+
+variable "github_repo" {
+  description = "GitHub repository name"
+  type        = string
+}
+
+variable "github_branch" {
+  description = "GitHub repository branch"
+  type        = string
+}
+
+variable "github_token" {
+  description = "GitHub token"
+  type        = string
+}
